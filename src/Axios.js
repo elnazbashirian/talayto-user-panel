e8feb98eff495c0b88868af5c1fd0663c3704416
elnaz bashirian <elnazbashiryann@gmail.com>
2023-10-28 14:22:32 +0330

    access-token deleted

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'http://api.talayto.com/v1';
-localStorage.setItem('access-token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MjY0YmJjNzEyMzhkMjAzNzA2YmM1YSIsImlhdCI6MTY5NzYzMTcxMiwiZXhwIjoxNzAxMjMxNzEyfQ.5b-9tJfYV92NmKsw7hEDN8mlNmIyj-IftfX3mlADBC0')
+// localStorage.setItem('access-token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MjY0YmJjNzEyMzhkMjAzNzA2YmM1YSIsImlhdCI6MTY5NzYzMTcxMiwiZXhwIjoxNzAxMjMxNzEyfQ.5b-9tJfYV92NmKsw7hEDN8mlNmIyj-IftfX3mlADBC0')
 axios.interceptors.request.use(function (config) {
     if (!localStorage.getItem("access-token")) {
         window.location.href = "http://www.talayto.com/login";
